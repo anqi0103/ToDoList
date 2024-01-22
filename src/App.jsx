bc055a3aa615d5921d41e72fd9d927711d61c15a
Anqi Lu <anqi0103@gmail.com>
2024-01-22 15:50:19 -0800

    Adding interactivity into my site

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,21 @@ import "./styles.css"
 
 export default function App() {
   const [newItem, setNewItem] = useState("")
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([])
 
   function handleSubmit(e) {
     e.preventDefault()
+
+    // Whenever we want to modify existing data, appending new element at the end of array instead of creating new
+    // array when we re-render the component, we need to pass a function to the setState, which will return the 
+    // currentValue you want the new state to be    
+    setTodos((currentTodo) => {
+      return [
+        ...currentTodo, 
+        {id: crypto.randomUUID(), title: newItem, completed: false}
+      ]
+    })
+    setNewItem("")
   }
 
   return <>
@@ -24,13 +35,17 @@ export default function App() {
     </form>
     <h1 className="header">Todo List</h1>
     <ul className="list">
-      <li>
-        <label>.
-          <input type="checkbox" />
-          Item 1
-        </label>
-        <button className="btn btn-danger">Delete</button>
-      </li>
+      {/* Using {} inside of HTML means that it is JavaScript code */}
+      {todos.map(todo => {
+        return <li key={todo.id}>
+          <label>
+            <input type="checkbox" defaultChecked={todo.completed}/>
+            {todo.title}
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      })}
+      
     </ul>
   </> 
 
